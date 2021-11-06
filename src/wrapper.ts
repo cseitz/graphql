cd5a9c9dc35495774a83336de4c3619962262a1e
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-11-06 17:11:15 -0400

    converted to tabs

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -40,76 +40,76 @@ export function ApolloCallbackWrapper(
 			...(info ? parseResolveInfo(info) : {}),
 		}
 		if (context && info) {
-            return cb(args, context, resolving)
-        }
-        return cb(parent, args, context, info)
+			return cb(args, context, resolving)
+		}
+		return cb(parent, args, context, info)
 	}
 }
 
 export function WrapApolloResolvers(resolvers: any) {
-    for (const resolverType in resolvers) {
-        const resolvers_ = resolvers[resolverType];
-        if (Object.keys(resolvers_).length == 0) {
-            delete resolvers[resolverType];
-        } else {
+	for (const resolverType in resolvers) {
+		const resolvers_ = resolvers[resolverType];
+		if (Object.keys(resolvers_).length == 0) {
+			delete resolvers[resolverType];
+		} else {
 
-            resolvers[resolverType] = Object.fromEntries(
-                Object.entries(resolvers_).map(([key, value]: [string, any]) => {
-                    return [key, ApolloCallbackWrapper(value, resolvers, resolverType)]
-                })
-            )
-        }
-    }
+			resolvers[resolverType] = Object.fromEntries(
+				Object.entries(resolvers_).map(([key, value]: [string, any]) => {
+					return [key, ApolloCallbackWrapper(value, resolvers, resolverType)]
+				})
+			)
+		}
+	}
 
-    return resolvers;
+	return resolvers;
 }
 
 
 //** Used in delayed computation of wrapped callbacks */
 /*
 class mutations extends ApolloResolverWrapper {
-    static collection = {
-        rename
-    };
+	static collection = {
+		rename
+	};
 }; new mutations();
 
 mutations.collection; // { rename: wrapped(rename) }
 */
 export class ApolloResolverWrapper {
-    [key: string]: any;
-    constructor(resolverType?: string) {
-        const groups = Object.getPrototypeOf(this).constructor;
-        if (!resolverType) {
-            const name = groups.name.toLowerCase();
-            if (name.includes('mut')) {
-                resolverType = 'Mutation'
-            } else if (name.includes('que')) {
-                resolverType = 'Query'
-            } else if (name.includes('sub')) {
-                resolverType = 'Subscription'
-            } else {
+	[key: string]: any;
+	constructor(resolverType?: string) {
+		const groups = Object.getPrototypeOf(this).constructor;
+		if (!resolverType) {
+			const name = groups.name.toLowerCase();
+			if (name.includes('mut')) {
+				resolverType = 'Mutation'
+			} else if (name.includes('que')) {
+				resolverType = 'Query'
+			} else if (name.includes('sub')) {
+				resolverType = 'Subscription'
+			} else {
 				resolverType = ''
 			}
-        }
-        for (const key in groups) {
-            const resolvers = groups[key];
-            const wrappedKey = '_' + key;
-            Object.defineProperty(groups, key, {
-                get() {
-                    if (wrappedKey in this) return this[wrappedKey];
-                    const _resolvers = { ...resolvers };
-                    for (const name in resolvers) {
-                        _resolvers[name] = ApolloCallbackWrapper(
-                            resolvers[name],
-                            _resolvers,
-                            resolverType as string
-                        )
-                    }
-                    this[wrappedKey] = _resolvers;
-                    return this[key];
-                }
-            })
-        }
-    }
+		}
+		for (const key in groups) {
+			const resolvers = groups[key];
+			const wrappedKey = '_' + key;
+			Object.defineProperty(groups, key, {
+				get() {
+					if (wrappedKey in this) return this[wrappedKey];
+					const _resolvers = { ...resolvers };
+					for (const name in resolvers) {
+						_resolvers[name] = ApolloCallbackWrapper(
+							resolvers[name],
+							_resolvers,
+							resolverType as string
+						)
+					}
+					this[wrappedKey] = _resolvers;
+					return this[key];
+				}
+			})
+		}
+	}
 }
 
